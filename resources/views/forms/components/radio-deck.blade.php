da1e22fa1d0cc14fff92ba3c7b2ad9f09d5ef8fc
Luuk Dahlmans <luukdahlmans@gmail.com>
2024-10-15 14:33:01 +0200

    Update radio-deck.blade.php

--- resources/views/forms/components/radio-deck.blade.php
+++ resources/views/forms/components/radio-deck.blade.php
@@ -102,6 +102,7 @@
                         <span class="font-medium text-gray-950 dark:text-white">
                             {{ $label }}
                         </span>
+                        @dd($hasPricing, $pricing, $hasTrial, $trial)
                         @if($hasPricing)
                             <span class="font-medium text-primary-600 mt-3 mb-3">
                                € {{$pricing}} <span style="font-size: 10px" class="text-gray-500 dark:text-white">/ month</span>
